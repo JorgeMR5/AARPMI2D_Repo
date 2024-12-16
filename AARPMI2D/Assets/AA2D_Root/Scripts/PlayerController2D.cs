3614abce29bd6fc420e49124f489bd710630b3fe
JorgeMR5 <144338977+JorgeMR5@users.noreply.github.com>
2024-12-16 17:14:04 +0100

    PlayerController

--- AARPMI2D/Assets/AA2D_Root/Scripts/PlayerController2D.cs
+++ AARPMI2D/Assets/AA2D_Root/Scripts/PlayerController2D.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController2D : MonoBehaviour
 {
+    //Referencias generales
+    [SerializeField] Rigidbody2D playerRb;
+    [SerializeField] Animator playerAnim;
+    [SerializeField] PlayerInput playerInput;
+    Vector2 moveInput;
+
+    [Header("Movement Stats")]
+    public float speed;
+    [SerializeField] bool isFacingRight = true;
+
+    [Header("Jump Stats")]
+    public float jumpForce;
+    [SerializeField] bool isGrounded;
+    [SerializeField] GameObject groundCheck;
+    [SerializeField] float groundCheckRadius = 0.1f;
+    [SerializeField] LayerMask groundLayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
@@ -15,4 +35,26 @@ public class PlayerController2D : MonoBehaviour
     {
         
     }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    void HandleAnimations()
+    {
+
+    }
+
+    #region Input Methods
+    public void HandleMovement(InputAction.CallbackContext context)
+    {
+
+    }
+    public void HandleJump(InputAction.CallbackContext context)
+    {
+
+    }
+
+    #endregion
 }
